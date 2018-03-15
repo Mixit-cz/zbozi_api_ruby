14cf36effbe8f8352752e000a04a70e2bc4b6b03
Jan Hovancik <conta.srdr@gmail.com>
2018-03-15 14:01:39 +0100

    Updates travis ci and adds coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'dotenv/load'
 require "zbozi_api_ruby"
 require "minitest/autorun"
 require 'vcr'
+require 'simplecov'
+
+SimpleCov.start
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/fixtures/vcr_cassettes"
